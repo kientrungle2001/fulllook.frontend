1cad26596b144875cd23bdfa5b50692e246dc5a3
huunv90 <nguyenhuu140490@gmail.com>
2018-07-27 15:48:29 +0700

    huunv

--- practice/detail.php
+++ practice/detail.php
@@ -100,8 +100,241 @@
 								<div class="btn full mb-3 lesson btn-primary">Bài 1</div>
 							</div>
 						</div>
-						<div class="alert alert-info">Bạn hãy chọn bài để luyện tập</div>
+						
+						<div class="do-practice full">
+							<div class="name-detail text-center">
+								Bài 1	
+							</div>						
+							<div class="text-center">
+								<div  class="time">
+									<img src="http://fulllook.com.vn/Themes/Songngu3/skin/images/watch.png">
+									<div id="countdown" class="num-time robotofont" style="color: rgb(255, 0, 0);">00:00</div>
+								</div>
+							</div>
+							
+							<div id="question" class="item">
+								<div class="question full">
+									<input type="hidden" name="questions[474]" value="474">
+									<input type="hidden" name="questionType[474]" value="choice">
+									<div class="item cau">
+										<div class="stt">Câu:  1</div>
+										<span class="btn volume fa fa-volume-up" onclick="read_question(this, '/3rdparty/Filemanager/source/practice/all/474.mp3');"
+										></span>
+									</div>
+
+									<div class="nobel-list-md choice">
+										<div class="ptnn-title full"> 
+												cho day so tu nhien	cho day so tu nhien	 cho day so tu nhien cho day so tu nhien		 
+										</div>
+									
+										<table>
+											<tbody>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+								
+										<a href="#mobile-explan-474" class="explanation top10 hidden btn btn-success btn-show-exp" data-toggle="collapse">Xem lý giải
+										</a>
+								
+										<div id="mobile-explan-474" class="collapse lygiai top10 item">
+											<div class="item mb-2">
+												ong huu		
+											</div>
+									
+											<div class="item">
+												<div class="btn btn-danger" data-toggle="modal" data-target="#report474">
+													Báo lỗi			
+												</div>
+										
+												<div class="modal fade" id="report474" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+												  <div class="modal-dialog" role="document">
+													<div class="modal-content">
+													  <div class="modal-header">
+														<button style="right: 15px;" type="button" class="close absolute" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+
+														<h4 class="modal-title" id="myModalLabel">Báo lỗi</h4>
+													  </div>
+													  <div class="modal-body">
+														 <div class="w100p">
+															<label for="exampleInputEmail1">Nội dung:</label>
+															<textarea style="height: 150px !important;" id="contentError474" name="contentError" class="form-control"></textarea>
+														  </div>
+											 
+													  </div>
+													  <div class="modal-footer">
+														
+														<button onclick="reportError(474);" type="button" class="btn btn-primary">Báo lỗi</button>
+													  </div>
+													</div>
+												  </div>
+												</div>
+										
+											</div>
+											<!--end report-->
+									
+										</div>
+										<!--Lý giải -->
+									</div>
+								</div>
+								<div class="line-question">
+								</div>
+								<!--end question-->
+
+								<div class="question full">
+									<input type="hidden" name="questions[474]" value="474">
+									<input type="hidden" name="questionType[474]" value="choice">
+									<div class="item cau">
+										<div class="stt">Câu:  1</div>
+										<span class="btn volume fa fa-volume-off" onclick="read_question(this, '/3rdparty/Filemanager/source/practice/all/474.mp3');"
+										></span>
+									</div>
+
+									<div class="nobel-list-md choice">
+										<div class="ptnn-title full"> 
+												cho day so tu nhien	cho day so tu nhien	 cho day so tu nhien cho day so tu nhien		 
+										</div>
+									
+										<table>
+											<tbody>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<input type="radio" name="answers[474]" id="answers_474_38915" class="float-left" value="38915">
+														<span class="answers_474_38915 pl10">
+															38			
+														</span>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+								
+										<a href="#mobile-explan-474" class="explanation top10 hidden btn btn-success btn-show-exp" data-toggle="collapse">Xem lý giải
+										</a>
+								
+										<div id="mobile-explan-474" class="collapse lygiai top10 item">
+											<div class="item mb-2">
+												ong huu		
+											</div>
+									
+											<div class="item">
+												<div class="btn btn-danger" data-toggle="modal" data-target="#report474">
+													Báo lỗi			
+												</div>
+										
+												<div class="modal fade" id="report474" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+												  <div class="modal-dialog" role="document">
+													<div class="modal-content">
+													  <div class="modal-header">
+														<button style="right: 15px;" type="button" class="close absolute" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+
+														<h4 class="modal-title" id="myModalLabel">Báo lỗi</h4>
+													  </div>
+													  <div class="modal-body">
+														 <div class="w100p">
+															<label for="exampleInputEmail1">Nội dung:</label>
+															<textarea style="height: 150px !important;" id="contentError474" name="contentError" class="form-control"></textarea>
+														  </div>
+											 
+													  </div>
+													  <div class="modal-footer">
+														
+														<button onclick="reportError(474);" type="button" class="btn btn-primary">Báo lỗi</button>
+													  </div>
+													</div>
+												  </div>
+												</div>
+										
+											</div>
+											<!--end report-->
+									
+										</div>
+										<!--Lý giải -->
+									</div>
+								</div>
+								<div class="line-question">
+								</div>
+								<!--end question-->
+
+								
+							</div>
+
+							<div class="text-center full mb-3 relative">				
+								<button id="finish-choice" class="btn btn-primary btt-practice " name="finish-choice" onclick="finish_choice();"><span class="fa fa-check"></span>
+									Hoàn thành					
+								</button>
+								<button id="view-result" class="btn btt-practice btn-success" data-toggle="modal" data-target="#exampleModal" name="view-result" style=""><span class="fa fa-list-alt"></span>
+									Xem kết quả					
+								</button>
+								<button id="show-answers" class="btn btt-practice btn-danger " name="show-answers" onclick="show_answers();"  ><span class="fa fa-check"></span>
+								Xem đáp án					
+								</button>
+							</div>
+
+							
+
+						</div>			
+
 					</div>
+					
+					<div style="height: 103px;" class="relative item">
+						<img style="left: 0px; bottom: 0px; border-radius: 0px 0px 0px 5px;" class="absolute hidden-xs" src="/assets/images/bottom-left.png">
+						<img style="right: 0px; bottom: 0px; border-radius: 0px 0px 5px 0px;" class="absolute hidden-xs" src="/assets/images/bottom-right.png">
+						
+					</div>	
 				</div>
 			</div>
 		</div>
