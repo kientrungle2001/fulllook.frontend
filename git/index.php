551398e8f2706f5f232a156830514b3a0eaf9276
huunv90 <nguyenhuu140490@gmail.com>
2018-08-02 15:47:00 +0700

    huunv

--- git/index.php
+++ git/index.php
@@ -1,1 +1,160 @@
+<style>
+	.fix-menu{margin-bottom: 15px;}
+</style>
+<div class="full pt-3">
+	<div class="container">
+		<div class="full mb-3">
+			<div class="row">
+				<div class="col-12 col-md-5">
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					 
+					  	<div class="carousel-inner">
 
+						    <div class="carousel-item active">
+						      <img class="d-block  w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/e5ccdb1fdff7e850350cb1c46f22a7ed.jpg" alt="First slide">
+						      <div class="carousel-caption d-none d-md-block">
+							    <h5>Summer Time</h5>
+							  </div>
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" alt="Second slide">
+						      <div class="carousel-caption d-none d-md-block">
+							    <h5>Circle of Life (from ...      </h5>
+							  </div>
+						    </div>
+							    
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							    <span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+							    <span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>	
+				</div>
+				<div class="col-12 col-md-7">
+					<div class="row">
+						<div class="col-12 col-md-8">
+							<div class="row mt-2">
+								<div class="col-md-6 col-12 mb-3">
+
+									<img class="d-block  w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/e5ccdb1fdff7e850350cb1c46f22a7ed.jpg" alt="First slide">
+								    <div class="carousel-caption d-none d-md-block">
+									    <h5>Summer Time</h5>
+									</div>
+
+								</div>
+								<div class="col-md-6 col-12 mb-3">
+									<img class="d-block w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" alt="Second slide">
+							       <div class="carousel-caption d-none d-md-block">
+								    <h5>Circle of Life (from ...      </h5>
+								   </div>
+								</div>
+								<div class="col-md-6 col-12 mb-3">
+									<img class="d-block w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" alt="Second slide">
+							       <div class="carousel-caption d-none d-md-block">
+								    <h5>Circle of Life (from ...      </h5>
+								   </div>
+								</div>
+								<div class="col-md-6 col-12 mb-3">
+									<img class="d-block w-100" src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" alt="Second slide">
+							       <div class="carousel-caption d-none d-md-block">
+								    <h5>Circle of Life (from ...      </h5>
+								   </div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-12">
+							<div class="row box-document mb-1">
+								<div class="col-md-8">
+								<p class="top10">FULL LOOK</p>
+								</div>
+								<div class="col-md-4">
+								<a href="http://s1.nextnobels.com"><img class="image-responsive center-block right-img" src="http://s1.nextnobels.com/default/skin/nobel/test/themes/default/media/full.png"></a>
+								</div>
+							</div>
+							<div class="row box-document mb-1">
+								<div class="col-md-8">
+								<p class="top10">Luyện viết văn miêu tả</p>
+								</div>
+								<div class="col-md-4">
+								<a href="http://nextnobels.com"><img class="image-responsive center-block right-img" src="http://s1.nextnobels.com/default/skin/nobel/test/themes/default/media/vietvan.png"></a>
+								</div>
+							</div>
+							<div class="row box-document mb-1">
+								<div class="col-md-8">
+								<p class="top10">Khảo sát năng lực toàn diện</p>
+								</div>
+								<div class="col-md-4">
+								<a href="http://nextnobels.com"><img class="image-responsive center-block right-img" src="http://s1.nextnobels.com/default/skin/nobel/test/themes/default/media/khaosat.png"></a>
+								</div>
+							</div>
+							<div class="row box-document mb-1">	
+								
+								<div class="col-md-8">
+								<p class="top10">Tiếng việt vui</p>
+								</div>
+								<div class="col-md-4">
+								<a href="http://nextnobels.com"><img class="image-responsive center-block right-img" src="http://s1.nextnobels.com/default/skin/nobel/test/themes/default/media/tiengvietvui.png"></a>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--end top git-->
+		<div class="mb-3 full">
+			<div class="row">
+				<div class="col-12 col-md-3 ">
+					<div class="full menu-git bg-info text-white p-2 text-center">Bài hát</div>
+				</div>
+				<div class="col-12 col-md-3 ">
+					<div class="full menu-git bg-info text-white p-2 text-center">Thơ</div>
+				</div>
+				<div class="col-12 col-md-3 ">
+					<div class="full menu-git bg-info text-white p-2 text-center">Truyện</div>
+				</div>
+				<div class="col-12 col-md-3 pr-0">
+					<div class="full menu-git bg-info text-white p-2 text-center">Bài viết</div>
+				</div>
+			</div>
+		</div>
+		<!--end menu-git-->
+		<div class="mb-3 full">
+			<div class="row">
+				<div class="col-md-4 col-12">
+					<a onclick="chitiet(184); return false;" href="">
+						<img src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" class="img-fluid">
+					</a>
+				</div>
+				<div class="col-md-8 col-12 ">
+					<h4><a onclick="chitiet(184); return false;" href="">Circle of Life (from the Lion King)</a></h4>
+							<p>How do you understand the phrase "cirlce of life"? What most impresses you in this video?</p>
+					<button class="btn btn-success" onclick="chitiet(184); return false;">Detail</button>
+				</div>
+			</div>
+			<div class="bdbot mt-3 mb-3"></div>
+
+			<div class="row">
+				<div class="col-md-4 col-12">
+					<a onclick="chitiet(184); return false;" href="">
+						<img src="http://s1.nextnobels.com/3rdparty/uploads/images/55896bd722d3504fda94c38a17603ec1.png" class="img-fluid">
+					</a>
+				</div>
+				<div class="col-md-8 col-12 ">
+					<h4><a onclick="chitiet(184); return false;" href="">Circle of Life (from the Lion King)</a></h4>
+							<p>How do you understand the phrase "cirlce of life"? What most impresses you in this video?</p>
+					<button class="btn btn-success" onclick="chitiet(184); return false;">Detail</button>
+				</div>
+			</div>
+			<div class="bdbot mt-3 mb-3"></div>
+
+		</div>
+
+
+		
+	</div>
+</div>	
