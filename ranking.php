7eb542bf9fa4208f4125675b0047690d7e3fe10c
Le Trung Kien <kientrungle2001@gmail.com>
2018-08-14 09:31:27 +0700

    dont know

--- ranking.php
+++ ranking.php
@@ -1,5 +1,5 @@
 <?php require_once 'bootstrap.php';?><!DOCTYPE html>
-<html ng-app="flApp" ng-controller="DocumentController">
+<html ng-app="flApp" ng-controller="TestRankingController">
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,14 +9,20 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome-4.6.3/css/font-awesome.min.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	
 	<script>
 		FL_API_URL = '<?php echo FL_API_URL?>';
+		test_id = '<?php echo intval($_GET['test_id']); ?>';
+		serverTime = <?php echo time() ?>;
+		setInterval(function() {
+			serverTime++;
+		}, 1000);
 	</script>
 </head>
 <body>
 	<?php include('common/header.php'); ?>
-	<?php include('document/index.php'); ?>
+	<?php include('test/ranking.php'); ?>
 	<?php include('common/footer.php'); ?>
-	<script src="/assets/angular/document.js"></script>
+	<script src="/assets/angular/testRanking.js"></script>
 </body>
 </html>
