a4d4f2fc6c7a887c5a8e2964fa6b3dfbccd8867d
Le Trung Kien <kientrungle2001@gmail.com>
2018-08-14 17:06:11 +0700

    ranking

--- assets/angular/testRanking.js
+++ assets/angular/testRanking.js
@@ -1,4 +1,4 @@
-flApp.controller('TestController', ['$scope', function($scope) {
+flApp.controller('TestRankingController', ['$scope', function($scope) {
 	$scope.title = 'Công ty cổ phần giáo dục và phát triển trí tuệ sáng tạo Next Nobels';
 	$scope.language = window.localStorage.getItem('language') || 'en';
 	$scope.changeLanguage = function() {
@@ -32,11 +32,50 @@ flApp.controller('TestController', ['$scope', function($scope) {
 		}
 	});
 	$scope.step = 'selectTest';
+	$scope.numberRow = 10;
+	$scope.numberPage = 0;
 	$scope.selectTest = function(test) {
 		$scope.step = 'selectTest';
-		$scope.testStep = 'selectTest';
-		$scope.showAnswersStep = 'selectTest';
 		$scope.selectedTest = test;
+		$scope.selectPage(1);
 	};
+
+	$scope.selectPage = function(numberPage) {
+		$scope.numberPage = numberPage - 1;
+		jQuery.ajax({
+			type: 'post',
+			url: FL_API_URL +'/rating/getRating', 
+			data: {
+				testId: $scope.selectedTest.id,
+				numberRow: $scope.numberRow,
+				numberPage: $scope.numberPage
+			},
+			dataType: 'json',
+			success: function(resp) {
+				$scope.rankings = resp;
+				$scope.pages = Math.ceil(resp.total / $scope.numberRow);
+				$scope.$apply();
+			}
+		});
+	};
+
+	$scope.range = function(min, max, step) {
+		step = step || 1;
+		var input = [];
+		for (var i = min; i <= max; i += step) {
+			input.push(i);
+		}
+		return input;
+	};
+
+	$scope.pageRange = function(min, max, step) {
+		step = step || 1;
+		var input = [];
+		for (var i = min; i <= max; i += step) {
+			if(i > $scope.numberPage - 4 && i < $scope.numberPage + 6 || i == min || i == max)
+			input.push(i);
+		}
+		return input;
+	}
 
 }]);
