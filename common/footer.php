427a23f087785ecf768f960fc6450facc6acd718
huunv90 <nguyenhuu140490@gmail.com>
2018-08-28 14:55:02 +0700

    huunv

--- common/footer.php
+++ common/footer.php
@@ -53,6 +53,8 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.2/angular.min.js" integrity="sha256-ruP2+uorUblSeg7Tozk75u8TaSUKRCZVvNV0zRGxkRQ=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.2/angular-sanitize.min.js" integrity="sha256-FnMl43xSx3jHmcs7t2LJ3htfsCeo99XORPvzOVQN/tw=" crossorigin="anonymous"></script>
 	<script src="/assets/array.js"></script>
+	<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>
+	<script src="/assets/angular/app.js"></script>
 
 	<script>
 		jQuery(function(){
@@ -61,10 +63,6 @@
 		    });
 			jQuery(".nav-item.dropdown").hover(            
 				function() {
-					//jQuery(this).find('a.dropdown-toggle').trigger('click');
-					// jQuery(".nav-item.dropdown").removeClass('show');
-					// jQuery('ul.dropdown-menu').removeClass('show');
-					// jQuery(this).find('a.dropdown-toggle').attr('aria-expanded',"true");
 					jQuery(this).addClass('show');
 					jQuery(this).find('ul.dropdown-menu').css('top', '95%');
 					jQuery(this).find('ul.dropdown-menu').addClass('show');        
@@ -90,8 +88,10 @@
 	                controller: ["$scope", "$element", "$attrs",
 	                    function($scope, $element, $attrs) {
 	                        $scope.$watch($attrs.mathjaxBind, function(texExpression) {
-	                            $element.html(texExpression);
-	                            MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+								$element.html(texExpression);
+								if(typeof MathJax != 'undefined'){
+									MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+								}
 	                        });
 	                    }]
 	            };
@@ -100,10 +100,8 @@
 	</script>
 
 
-	<script src="/assets/angular/app.js"></script>
 
 	
-	<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>
 	<script type="text/x-mathjax-config"> 
 		MathJax.Hub.Config({
 			showMathMenu: false,
