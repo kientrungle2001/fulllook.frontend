8a1729b03eec9a2bc7ccefa0d5609067522734c3
Le Trung Kien <kientrungle2001@gmail.com>
2018-08-29 11:14:11 +0700

    all

--- assets/angular/practice.js
+++ assets/angular/practice.js
@@ -36,8 +36,8 @@ flApp.controller('PracticeController', ['$scope', function($scope) {
 	$scope.parseTranslate = function(str) {
 		if(str) {
 			str = str.replace(/\[start\](.*)\[end\]/g, `<button class="btn btn-primary" data-toggle="collapse" onclick="jQuery(this).next().collapse('toggle')">Dịch</button><div class="collapse"><div class="card card-body">$1</div></div>`);
-			str = str.replace(/\[fix\](.*)\[endfix\]/g, `<span class="btn btn-default fa fa-volume-up" onclick="read_question(this,'http://s1.nextnobels.com/3rdparty/Filemanager/source/audiovocabulary/'+'$1'.toLowerCase()+'.mp3');"></span>`);
-			str = str.replace(/\[audio\](.*)\[endaudio\]/g, `$1<span class="btn btn-default fa fa-volume-up" onclick="read_question(this,'http://s1.nextnobels.com/3rdparty/Filemanager/source/audiovocabulary/'+'$1'.toLowerCase()+'.mp3');"></span>`);			
+			str = str.replace(/\[fix\](.*)\[endfix\]/g, `<span class="btn btn-default fa fa-volume-up" onclick="read_question(this,'http://s1.nextnobels.com/3rdparty/Filemanager/source/audiovocabulary/'+'$1'.toLowerCase().replace(/ /g, '_')+'.mp3');"></span>`);
+			str = str.replace(/\[audio\](.*)\[endaudio\]/g, `$1<span class="btn btn-default fa fa-volume-up" onclick="read_question(this,'http://s1.nextnobels.com/3rdparty/Filemanager/source/audiovocabulary/'+'$1'.toLowerCase().replace(/ /g, '_')+'.mp3');"></span>`);			
 			return str;
 		};
 		return '';
